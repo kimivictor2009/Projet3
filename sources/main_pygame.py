5af72e6e9415ac49fc89df15a13f452548d896d4
KVTeam <kimivictor@proton.me>
2026-03-08 19:11:48 +0100

    Refactor code and implement main menu with intro
    Updated the code structure and added a main menu with an intro. Changed variable names to English and added a ticking mechanism for animations.

--- sources/main_pygame.py
+++ sources/main_pygame.py
@@ -1,9 +1,24 @@
 '''
-
 ==================== KOVALENT ====================
+
+Bandeau d'informations - tenir à jour !
+
+Version : 1.4
+
+Dernière édition : Victor, 08/03/2025, 19:11
+
+
+---------- COMMENTAIRE ----------
+
+J'ai trouvé un moyen de dessiner les atomes et liaisons,
+mais c'était juste pour remplir la fonction render
+Je l'ai supprimé pour faire le menu
+J'ai mis tous les noms de trucs en anglais
+La variable tick augmente de 1 à chaque frame, pour faire des animations
 
 
 ---------- NOTES ----------
+
     Note pour le stockage des atomes :
         atomes = [{id : 3, nom : "C", pos : (x=127, y=208), liaisons : [(id=5, nb_liaisons=2), (etc)]}, {atome2}, {etc}]
 
@@ -12,6 +27,11 @@
         liste des atomes
         atome = dictionnaire avec infos
         infos = id, formule, position et liaisons
+    
+    Penser à nommer les variables en anglais pour la réutilisation
+    Penser aussi aux docstrings et aux commentaires
+    Pas besoin de faire *SCALE pour du texte, la fonction le fait automatiquement
+
 
 ---------- HISTORIQUE DES MODIFICATIONS ----------
 
@@ -22,10 +42,20 @@
     -> Version 1.1, Victor
         - Fonction render() pour tester
         - Affichage atomes et liaisons (TEST, sera supprimé)
+    -> Version 1.2, Victor
+        - Fonctions create_text() et print_text(), permettant d'afficher du texte dans la fenêtre
+        - Structuration du code
+    -> Version 1.3
+        - Dimensionnage automatique de la fenêtre, qui s'adapte à l'écran principal
+        - Variable fenetre_basique à changer pour avoir une fenêtre de dimension prédéfinie (pour les devs)
+    -> Version 1.4
+        - Ajout d'un ticker
+        - Ajout d'une intro
+        - Ajout d'un titre du menu
 
 ==================== main.py ====================
-
 '''
+
 
 # -----<===== INITIALISATION =====>-----
 
@@ -33,25 +63,60 @@
 
 from __future__ import annotations
 import pygame as pg
+from screeninfo import get_monitors
 
+# ----- Couleurs, constantes et variables/tableaux/autres -----
 
-# ----- Couleurs, constantes et initialisation de pygame -----
+fenetre_basique = True
 
-# Test pour une molécule de CH2O
-atomes = [{"id" : 1, "nom" : "C", "pos" : (127, 208), "liaisons" : [(2, 2), (3, 1), (4, 1)]},
-          {"id" : 2, "nom" : "O", "pos" : (94, 215), "liaisons" : [(1, 2)]},
-          {"id" : 3, "nom" : "H", "pos" : (131, 249), "liaisons" : [(1, 1)]},
-          {"id" : 4, "nom" : "H", "pos" : (134, 157), "liaisons" : [(1, 1)]}]
+tick = 0
+
+# Test pour une molécule de CH2O (chaque ligne est un atome)
+atoms = [{"id" : 1, "name" : "C", "pos" : (127, 208), "links" : [(2, 2), (3, 1), (4, 1)]},
+          {"id" : 2, "name" : "O", "pos" : (94, 215), "links" : [(1, 2)]},
+          {"id" : 3, "name" : "H", "pos" : (131, 249), "links" : [(1, 1)]},
+          {"id" : 4, "name" : "H", "pos" : (134, 157), "links" : [(1, 1)]}]
 
 
 BLACK = (0, 0, 0)
+DARK_GREY = (100, 100, 100)
 WHITE = (255, 255, 255)
+YELLOW = (200, 200, 0)
 
 
-SCALE = 100 # Taille de la fenêtre en %, tout doit être proportionnel - peut être modifié à volonté
+# ----- Initialisation de pygame et création de la fenêtre -----
 
-WINDOW_LENGHT = SCALE*6
-WINDOW_HEIGHT = SCALE*6
+
+WINDOW_HEIGHT = 700
+WINDOW_LENGHT = 1050
+
+PROP_WINDOW = 1.5  # la proportion de la longueur sur la hauteur
+
+if not fenetre_basique :
+    
+    # Parcoure les moniteurs et prend le principal
+    for m in get_monitors() :
+        if m.is_primary : # Le moniteur principal
+            
+            if (m.height - 150)*1.5 <= m.width : # Si la fenêtre est assez large par rapport à ce qu'on veut si on prend la hauteur en modèle
+                
+                WINDOW_HEIGHT = m.height - 150 # le 150 est une distance entre le bord haut et bas et la fenêtre
+                WINDOW_LENGHT = WINDOW_HEIGHT * PROP_WINDOW
+                
+            else :  # Sinon
+                
+                WINDOW_LENGHT = m.width - 50
+                WINDOW_HEIGHT = WINDOW_LENGHT / PROP_WINDOW
+    
+
+
+SCALE = WINDOW_HEIGHT/800
+# Correspond à la taille de la fenêtre, tout doit être proportionnel
+# SCALE vaut 1.0 si la fenêtre à une hauteur (par défaut) de 800px, ce qui correspond à celle de fenetre_basique
+# ATTENTION C'EST UN FLOAT
+# Pas besoin de faire *SCALE pour du texte, la fonction le fait automatiquement
+
+
 
 WINDOW_SIZE = (WINDOW_LENGHT, WINDOW_HEIGHT)
 
@@ -60,26 +125,78 @@ pg.init()
 surface = pg.display.set_mode(WINDOW_SIZE)
 pg.display.set_caption("Kovalent")
 
+# ----- Fonts -----
+
+pg.font.init()
+
+def create_text(text : str, size : int, color : tuple = WHITE) -> pg.Surface :
+    '''Renvoie une Surface de texte, à blit pour afficher'''
+    
+    font = pg.font.Font(pg.font.get_default_font(), size)
+    return font.render(text, True, color)
+
+
+def print_txt(text : str, pos : tuple, size : int = 30, color : tuple = WHITE, dest = surface) -> None :
+    '''Affiche du texte dans la fenêtre, mis automatiquement à son échelle'''
+    
+    dest.blit(create_text(text, int(size*SCALE), color), (int(pos[0]*SCALE), int(pos[1])*SCALE))
+
+
+
+# -----<===== FONCTIONS PRINCIPALES =====>-----
+
 
 def render() -> None :
-    for atome in atomes :
-        pg.draw.circle(surface, WHITE, (atome["pos"][0]*2, atome["pos"][1]*2), 10)
-        for liaison in atome["liaisons"] :
-            for atome2 in atomes :
-                if atome2["id"] == liaison[0] :
-                    pg.draw.line(surface, WHITE, (atome["pos"][0]*2, atome["pos"][1]*2), (atome2["pos"][0]*2, atome2["pos"][1]*2), 5*liaison[1])
+    '''Affiche tout ce qu'il faut afficher à l'écran'''
+    
+    # Les 200 premiers ticks sont pour une petite intro
+    
+    if tick < 200 :
+        if tick < 20 :
+            surface.fill(BLACK)
+        elif tick <= 80 :
+            surface.fill(BLACK)
+            teinte = ((tick-20)/60)*255
+            print_txt("KVTeam", (480, 350), 50, (teinte, teinte, teinte))
+        elif tick >= 140 :
+            surface.fill(BLACK)
+            teinte = (1-((tick-140)/60))*255
+            print_txt("KVTeam", (480, 350), 50, (teinte, teinte, teinte))
+    else :
+        surface.fill(DARK_GREY)
+        main_menu()
 
+
+
+def main_menu() -> None :
+    '''Affiche le menu principal'''
+    if tick < 300 :
+        print_txt("KOVALENT", (200, 50), 50)
+    else :
+        print_txt("KOVALENT", (100, 50), 50)
+
+
+
+# -----<===== BOUCLE PRINCIPALE =====>-----
+
+
+clock = pg.time.Clock()
 
 running = True
 
 while running == True :
+    
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            running = False
+            running = False   # Quitte la boucle quand l'évènement QUIT est détecté
     
-    surface.fill(BLACK)
+    
     render()
     pg.display.flip()
     
+    tick += 1 # + 1 ticks par frame (60 par seconde)
+    clock.tick(60) # Met le FPS à 60
+    
 pg.quit()
+
 
